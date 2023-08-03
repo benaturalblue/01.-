8a90b1cad6bde1423e6bd295f964012f8b86fb65
benaturalblue <yuuka.nakazaki513@gmail.com>
2023-08-03 12:38:56 +0900

    配列問題回答

--- lesson3.php
+++ lesson3.php
@@ -8,3 +8,11 @@
 // 20歳・女性
 
 // という形で出力してください。
+$person = array(
+  'name' => '山田',
+  'age' => 20,
+  'gender' => '女性'
+);
+echo $person['name'] . "\n";
+echo $person['age'] . '歳・' . $person['gender'];
+?>
